a6f6891946fd54d598b7fc446ded74533ca346ed
EB <e.bains@hotmail.co.uk>
2023-03-28 17:59:40 +0100

    Worked with Roger (Course instructor) during office hours to get timer to work, currently starts on the main page.

---
+++ assets/js/questions.js
@@ -0,0 +1,46 @@
+//This file for questions only- prevent default required to move on to the next question?
+
+
+//Question 1- HTML
+
+var htmlEl = document.createElement("div");// key pair/ listener to work?
+
+htmlEl.textContent = "1. </body> Is this an opening HTML tag or a closing HTML tag?:";
+
+var listEl = document.createElement("ol"); // creates ordered list?
+
+
+//list for answer options?
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+
+//answer options
+li1.textContent = "Opening HTML tag";
+li2.textContent = "Closting HTML tag"; // Correct answer- not sure how to show it
+
+//htmlEl.appendChild(listEl);  - need a parent for this to work 
+
+
+// Go back to lessom 1 secton 8 to add in the parent child strucutre if it doesn't look right on the page
+
+//Question 2
+
+//Question 3
+//Question 4
+//Question 5
+
+// Buttons- lesson 2 act 2
+
+//Event bubbles- lesson 2 ex8 not sure if needed as once the option has been slectedto move on t the next questins
+
+// local storage user- forenetring the intials- lesson 3 act 2
+
+
+
+
+
+
+
+
+
+
